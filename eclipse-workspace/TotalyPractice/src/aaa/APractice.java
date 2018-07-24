6b2e39b7d2cd033d0fa0f0bd461a52576c3c26f7
acakici <acakici72@gmail.com>
2018-07-24 15:55:02 -0500

    Initial

---
+++ eclipse-workspace/TotalyPractice/src/aaa/APractice.java
@@ -0,0 +1,16 @@
+package aaa;
+
+
+
+//public class APractice {
+	
+	public class APractice {
+		 static String result = "";
+		 { result += "c"; }
+		 static
+		 { result += "u"; }
+		 { result += "r"; }
+		 
+		 
+		 }
+	
---
+++ eclipse-workspace/TotalyPractice/src/bbb/BPractice.java
@@ -0,0 +1,12 @@
+package bbb;
+
+import aaa.APractice;
+
+public class BPractice {
+
+	public static void main(String[] args) {
+
+		
+	}
+
+}
